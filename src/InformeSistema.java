5c48870048a3696851b4a310eaa2e3fefc1356c5
Seta <amundaraysoares@danielcastelao.org>
2026-09-17 14:38:06 +0200

    tarea de psp a la mitad

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,23 +1,85 @@
+import java.io.File;
 import java.io.IOException;
 import java.lang.Runtime;
-
+import java.util.Properties;
 public class InformeSistema {
 
-public static int NumProcesos(){
+public static void NumProcesos() {
     int procesos = Runtime.getRuntime().availableProcessors();
-    return procesos;
+    System.out.println(" PROCESADORES");
+    System.out.println("============================================================");
+    System.out.println("\tDisponible JMV: "+ procesos+ "mib");
+    System.out.println("\t(son hilos lógicos: con SMT no coincide con los núcleos físicos)");
+    System.out.println(" ");
 }
-public  static int Memoria(){
+public  static void Memoria(){
+    long mtotal = Runtime.getRuntime().totalMemory();
+    long mFree = Runtime.getRuntime().freeMemory();
+    long mMax = Runtime.getRuntime().maxMemory();
+    long mUse = mtotal - mFree;
+
+    long mib = (1024 * 1024);
+
+    long totalMiB = mtotal / mib;
+    long MAximaMib = mMax / mib;
+    long freeMiB = mFree / mib;
+    long useMiB = mUse / mib;
+    long mPorcentaje = (mUse * 100l) / mtotal;
+
+
+    System.out.println("MEMORIO . ANTES");
+    System.out.println("============================================================");
+    System.out.println("\tTotal reservada: "+totalMiB+" mib");
+    System.out.println("\tLibre: "+freeMiB+" mib");
+    System.out.println("\tEn uso: "+useMiB+" mib " +"("+ mPorcentaje +"% de la total)");
+    System.out.println("\tMáxima (-Xmx): "+MAximaMib+" mib");
+    System.out.println(" ");
+
+    long[] reservado = new long[8 * 1024 * 1024];
+    if (reservado.length > 0){
+        reservado[0] = 1L;
+    }
+    long dmtotal = Runtime.getRuntime().totalMemory();
+    long dmFree = Runtime.getRuntime().freeMemory();
+    long dmMax = Runtime.getRuntime().maxMemory();
+    long dmUse = dmtotal - dmFree;
+
+    long dtotalMiB = dmtotal / mib;
+    long dMAximaMib = dmMax / mib;
+    long dfreeMiB = dmFree / mib;
+    long duseMiB = dmUse / mib;
+    long dmPorcentaje = (dmUse * 100l) / dmtotal;
+
+    System.out.println("MEMORIO . DESPUES");
+    System.out.println("============================================================");
+    System.out.println("\tTotal reservada: "+dtotalMiB+" mib");
+    System.out.println("\tLibre: "+dfreeMiB+" mib");
+    System.out.println("\tEn uso: "+duseMiB+" mib " +"("+ dmPorcentaje +"% de la total)");
+    System.out.println("\tMáxima (-Xmx): "+dMAximaMib+" mib");
+}
+
+public static void SISTEMA(){
+   String SO =  System.getProperty("os.name");
+   String separador = File.separator;
+    String ruta = System.getProperty("user.home");
+    System.out.println("SISTEMA");
+    System.out.println("============================================================");
+    System.out.println("\tos.name: "+SO);
+    System.out.println("\tfile.separator: "+separador);
+    System.out.println("\tRuta construida con las propiedades: "+ruta+"psp"+separador+"informe.txt");
 
 }
+    public static void PROPIEDADES(){
 
+
+    }
 
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println(
-                InformeSistema.NumProcesos()
-        );
+        InformeSistema.NumProcesos();
+        InformeSistema.Memoria();
+        InformeSistema.SISTEMA();
     }
 
 
